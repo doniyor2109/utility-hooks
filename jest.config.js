8fa2ce1087e646afd15e5446639bb8563cba71e4
Umidbek Karimov <uma.karimov@gmail.com>
2020-03-27 11:49:21 +0500

    chore: Bump dependencies.

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,8 @@
 'use strict';
 
 module.exports = {
-  // An array of regexp pattern strings used to skip coverage collection
-  coveragePathIgnorePatterns: ['/pkg/', '/node_modules/'],
+  // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
+  testPathIgnorePatterns: ['/pkg/', '/node_modules/'],
 
   // The glob patterns Jest uses to detect test files
   testMatch: ['**/__tests__/**/*.spec.[jt]s?(x)'],
